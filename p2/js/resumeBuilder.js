6a1d22deceda5730fecad575768144eb0026a726
Chris Tirpak <ctirpak1@gmail.com>
2014-11-05 23:09:04 -0500

    added some content

---
+++ p2/js/resumeBuilder.js
@@ -0,0 +1,33 @@
+var bio = {
+	"name" : "Chris Tirpak",
+	"role" : "Web Developer",
+	"contact" : "ctirpak1@gmail.com",
+	"mobile" : "908-512-6356",
+	"email" : "ctirpak1@gmail.com",
+	"twitter" : "<a href='https://twitter.com/c_tirpak'>@c_tirpak</a>",
+	"github" : "<a href='https://github.com/ctirpak' alt='bio picture'>https://github.com/ctirpak</a>",
+	"location" : "Apex, NC",
+	"pictureURL" : "images/skull.jpg",
+	"welcomeMsg" : "Welcome to my resume",
+	"skills" : ["HTML5", "JavaScript", "jQuery", "SQL Server", "ASP.NET", "VBA"]
+};
+
+var formattedName = HTMLheaderName.replace("%data%", bio.name);
+var formattedRole = HTMLheaderRole.replace("%data%", bio.role);
+var formattedContact = HTMLcontactGeneric.replace("%data%", bio.contact);
+var formattedMobile = HTMLmobile.replace("%data%", bio.mobile);
+var formattedEmail = HTMLemail.replace("%data%", bio.email);
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.twitter);
+var formattedGithub = HTMLgithub.replace("%data%", bio.github);
+var formattedLocation = HTMLlocation.replace("%data%", bio.location);
+var bioPic = HTMLbioPic.replace("%data%",bio.pictureURL);
+var formattedWelcomeMsg = HTMLbioPicHTMLWelcomeMsg.replace("%data%",bio.welcomeMsg);
+
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
+$("#header").prepend(bioPic);
+$("#header").append(formattedWelcomeMsg);
+$("#footerContacts").append(formattedMobile);
+$("#footerContacts").append(formattedEmail);
+$("#footerContacts").append(formattedTwitter);
+$("#footerContacts").append(formattedGithub);
