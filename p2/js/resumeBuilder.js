4cebb615237560e26105083053741b48d88238b6
Chris Tirpak <ctirpak1@gmail.com>
2014-11-11 11:09:16 -0500

    added new jquery ui components and updated resume

--- p2/js/resumeBuilder.js
+++ p2/js/resumeBuilder.js
@@ -16,17 +16,52 @@ var work = {
 	"jobs": [
 		{
 			"employer": "AT&T",
-			"position": "Manager",
-			"years": "2004 to present",
-			"city": "Raleigh, NC",
-			"description": "Application developer. Design and build applications for a user base of 500+ employees. Applications built using HTML, JavaScript, jQuery, ASP.NET, Excel VBA, SQL Server."
+			"position": "Sr Consultant Sys Integration - Business Solutions Group",
+			"years": "2008 - Present",
+			"city": "Raleigh NC",
+			"description": "Operational Effectiveness. Identify, analyze and improve organizational value by providing both new and enhanced solutions that help increase and optimize productivity, efficiency and effectiveness. Responsible for analysis and creation of processes and custom applications to help improve organizational productivity. Software used includes: HTML5, JavaScript, JQuery, SQL Server, ASP.NET, Visual Basic for Applications (VBA) for Microsoft Office, SharePoint Designer, Microsoft Access, Microsoft Excel, Visual Basic"
 		},
 		{
 			"employer": "AT&T",
-			"position": "Manager",
-			"years": "1990 to 2003",
-			"city": "Raleigh, NC",
-			"description": "Budget Analysist. Manage $1.1B budget. Manage budgets and expenses for 1,200 Local Exchange Companies (LECs) to aid in contract negotiations."
+			"position": "Manager - Business Marketing - Wireless",
+			"years": "2006 - 2007",
+			"city": "Piscataway NJ",
+			"description": "Wireless and VoIP management.  Includes budget development, financial tracking and results reporting to management, inventory management, purchase order tracking.  Gather data and develop forecasts based on trending, business assumptions, and sales data.  Advanced use of Microsoft Office (Excel, Access, PowerPoint, and Outlook) by using Visual Basic for Applications (VBA) to automate database updates and report generation.  Develop VBA front-end interfaces for Excel and Access to allow user input, real-time data manipulation, and report generation (either printed or exported to other applications)."
+		},
+		{
+			"employer": "AT&T",
+			"position": "Staff Manager - Consumer Operations Solutions and Transformation (COS&T)",
+			"years": "2000 - 2006",
+			"city": "Piscataway NJ",
+			"description": "Manage financial budgets and outlooks within COS&T.  Develop budgets and provide business units with financial results.  Incorporate volume forecasts, conversion strategies and cost saving initiatives into budget and track monthly results to ascertain actual savings. Develop unit costs and board packages to determine financial impact of contractual changes.  Create monthly outlooks based on actual expenses, initiatives and conversion changes.  Analyze monthly actuals to explain variances against budget and outlook, prepare monthly financial results package for directorate. Advanced use of Excel (pivot tables, macros, charts) and Access (relational databases, SQL queries, reporting), OBATS (TSO/MVS system) used for budget development, actuals management, financial reporting and data transfer.  Supervision of two employees."
+		},
+		{
+			"employer": "AT&T",
+			"position": "Associate Manager - Billing Solutions and Services",
+			"years": "1998 - 2000",
+			"city": "Somerset NJ",
+			"description": "Manage financial budgets and outlooks within BS&S.  Develop budgets and provide business units with financial results.  Incorporate volume forecasts, conversion strategies and cost saving initiatives into budget and track monthly results to ascertain actual savings. Develop unit cost and board packages to determine financial impact of contractual changes.  Create monthly outlooks based on actual expenses, initiatives and conversion changes.  Analyze monthly actuals to explain variances against budget and outlook, prepare monthly financial results package for directorate. Hold monthly results and outlook meetings with VP, Division managers and CFO.  Advanced use of Excel (pivot tables, macros, charts) and Access (relational databases, SQL queries, reporting), OBATS (TSO/MVS system) used for budget development, actuals management, financial reporting and data transfer."
+		},
+		{
+			"employer": "AT&T",
+			"position": "Associate Manager - Local Service End-to-End Architecture",
+			"years": "1997 - 1998",
+			"city": "Piscataway NJ",
+			"description": "Analyze local systems architecture to determine capacity thresholds and pinch points using ProcessModel simulation software.  Identify problem areas within the current process and recommend alternative solutions to improve system response time and increase capacity.  Determine headcount requirements and system utilization based on current forecasts from BLS, CLS and ADL for local market entry.  Develop requirements for Resource Planning Model (an Excel-based planning tool) and work with programmers to implement changes.  Perform ‘what-if’ analysis with RPM and determine the impact of business strategies.  Prepare written documentation of systems procedures, simulation analysis and presentations. Software used: Excel, Access, Word, PowerPoint, ProcessModel, and Windows95."
+		},
+		{
+			"employer": "AT&T",
+			"position": "Associate Manager - LEC Billing Financial Management",
+			"years": "1993 - 1997",
+			"city": "Somerset NJ",
+			"description": "Responsible for the input and maintenance of financial budgets within LBFM systems.  Developed requirements for the allocation and assignment of LEC budget expenses to the BUs and SBUs.  Managed budget and actual variance, accruals and unit costs per bill, message and minute, applied conversion strategies to the budget and determined the financial impacts of cost saving initiatives. Created specifications for system enhancements, automated system processes through the use of macros.  Software and systems: Lotus, Approach, TSO, and Focus."
+		},
+		{
+			"employer": "AT&T",
+			"position": "Reports Clerk - LEC Billing Financial Management",
+			"years": "1987 - 1993",
+			"city": "East Brunswick NJ",
+			"description": "Accountable for the accurate input and processing of LEC B&C bills for payment by AT&T.  Generated financial reports and budget forecasts based on actual expenses.  Analyzed monthly costs to determine billing problems and resolved discrepancies with the LECs.  Software and systems: Lotus, Dbase, VM/CMS, and Relay."
 		},
 		{
 			"employer": "Alston School Photography",
