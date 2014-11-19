6c7a143e0249617cebeeaac81e4340811aef9fe2
Chris Tirpak <ctirpak@users.noreply.github.com>
2014-11-18 19:14:39 -0500

    added kibo files

--- p3/js/app.js
+++ p3/js/app.js
@@ -6,6 +6,8 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+	this.x=0;
+	this.y=0;
 }
 
 // Update the enemy's position, required method for game
@@ -24,12 +26,40 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
+// 
+// Player our enemies will kill
+var player = function() {
+    // The image/sprite for our player, this uses
+    // a helper we've provided to easily load images
+    this.sprite = 'images/char-boy.png';
+	this.x=0;
+	this.y=0;
+}
 
+// Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+}
+
+// Draw the player on the screen, required method for game
+player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// Handle input to move player
+player.prototype.handleInput = function() {
+  
+}
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var allEnemies = new Enemy();
+player = new player();
 
 
 // This listens for key presses and sends the keys to your
