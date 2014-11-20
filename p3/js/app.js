064eb4ded1fb0a926fbb51c0584cc68fffc5e19e
Chris Tirpak <ctirpak@users.noreply.github.com>
2014-11-19 19:20:43 -0500

    changed to inherited class objects

--- p3/js/app.js
+++ p3/js/app.js
@@ -1,18 +1,15 @@
-// Enemies our player must avoid
-var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
+// Superclass of objects in game (e.g. enemy, player)
+var Entity = function(s,r,c) {
+    // The image/sprite for our entity, this uses
     // a helper we've provided to easily load images
-    this.sprite = 'images/enemy-bug.png';
-	this.x=0;
-	this.y=0;
+    this.sprite = s;
+	this.x=c;
+	this.y=r;
 }
 
-// Update the enemy's position, required method for game
+// Update the entity's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Entity.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
@@ -23,55 +20,45 @@ Enemy.prototype.update = function(dt) {
 	this.y = this.y + ((50 - (Math.floor((Math.random() * 100) + 1)) ) * dt);
 }
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+// Draw the entity on the screen, required method for game
+Entity.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 }
 
-// Set the position. This is only used to set the starting position.
-// Movement is handled in the update() method
-Enemy.prototype.setPos = function(r, c) {
-  this.x=c;
-  this.y=r;
+
+// Enemies our player must avoid
+var enemy = function(s,r,c) {
+    // enemy is a sub class of Entity
+	// It does not have any additional methods
+    Entity.call(this, s, r, c);
 }
+
+// Need to set up prototype and constructor
+enemy.prototype = Object.create(Entity.prototype);
+enemy.constructor = enemy;
+
+
 
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
 // 
 // Player our enemies will kill
-var player = function() {
-    // The image/sprite for our player, this uses
-    // a helper we've provided to easily load images
-    this.sprite = 'images/char-boy.png';
-	this.x=0;
-	this.y=0;
+var plyr = function(s,r,c) {
+    // player is a sub class of Entity
+	// It does not have any additional methods
+    Entity.call(this, s, r, c);
 }
 
-// Update the player's position, required method for game
-// Parameter: dt, a time delta between ticks
-player.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-}
-
-// Draw the player on the screen, required method for game
-player.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-}
-
+// Need to set up prototype and constructor
+plyr.prototype = Object.create(Entity.prototype);
+plyr.constructor = plyr;
+/*
 // Handle input to move player
-player.prototype.handleInput = function() {
+plyr.prototype.handleInput = function() {
   
 }
-
-// Set the position. This is only used to set the starting position.
-// Movement is handled in the update() method
-player.prototype.setPos = function(r, c) {
-  this.x=c;
-  this.y=r;
-}
+*/
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
@@ -80,16 +67,13 @@ var allEnemies = [];
 var oneEnemy;
 
 for (var x =0;x<5;x++){
-  oneEnemy=new Enemy();
-  oneEnemy.setPos( ((x % 3) + 1) * 83, x * 101 );
-  
+  oneEnemy=new enemy('images/enemy-bug.png', ((x % 3) + 1) * 83, x * 101);
   allEnemies.push(oneEnemy);
 }
 
-player = new player();
-player.setPos(5 * 83, 101 * 2);
+var player = new plyr('images/char-boy.png', 5 * 83, 101 * 2);
 
-
+/*
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
 document.addEventListener('keyup', function(e) {
@@ -100,5 +84,6 @@ document.addEventListener('keyup', function(e) {
         40: 'down'
     };
 
-    player.handleInput(allowedKeys[e.keyCode]);
+    plyr.handleInput(allowedKeys[e.keyCode]);
 });
+*/
