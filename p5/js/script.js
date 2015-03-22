dc507e06118e99202b02e0112dfedc897b6ddbf2
Chris Tirpak <ctirpak@users.noreply.github.com>
2015-03-22 00:30:51 -0400

    moved info to dialog box

--- p5/js/script.js
+++ p5/js/script.js
@@ -1,106 +1,111 @@
 
 function loadData() {
 
-	var $body = $('body');
-	var $wikiElem = $('#wikipedia-links');
-	var $nytHeaderElem = $('#nytimes-header');
-	var $nytElem = $('#nytimes-articles');
-	var $greeting = $('#greeting');
-	var streetURL = 'https://maps.googleapis.com/maps/api/streetview?size=600x400';
-	var fullURL = '';
-	var fullAddress = '';
-	var nytURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json';
-	var nytParams = {
-		"api-key": "aeacc24cfc3061cdd0c6bba97bbba8ed:5:71450941",
-		"fl": "headline,snippet,web_url"
-	};
+    var $wikiElem = $('#wikipedia-links');
+    var $nytHeaderElem = $('#nytimes-header');
+    var $nytElem = $('#nytimes-articles');
+    var fullAddress = '';
+    var nytURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json';
+    var nytParams = {
+	"api-key": "aeacc24cfc3061cdd0c6bba97bbba8ed:5:71450941",
+	"fl": "headline,snippet,web_url"
+    };
 
 
-	// clear out old data before new request
-	$wikiElem.text("");
-	$nytElem.text("");
+    // clear out old data before new request
+    $wikiElem.text("");
+    $nytElem.text("");
 
-	// load streetview
-	fullAddress = $('#street').val() + $('#city').val();
-	fullURL = '<img class="bgimg" src="' + streetURL + '&location=' + fullAddress + '">';
-	// console.log(fullURL);
-	//$body.append(fullURL);
-	// YOUR CODE GOES HERE!
-	$.extend(nytParams, {'q': fullAddress});
-	// NY Times request
-	$.getJSON(nytURL, nytParams, function (data) {
-		var item = '';
+    // load streetview
+    fullAddress = $('#street').val();
+    $.extend(nytParams, {'q': fullAddress});
 
-		// add address as a parameter for ajax request
-		//console.log(data);
+    $('#history-items').append('<li>' + fullAddress + '</li>');
 
-		var articles = data.response.docs;
 
-		$nytHeaderElem.append(' for ' + fullAddress);
+    // NY Times request
+    $.getJSON(nytURL, nytParams, function (data) {
+	var item = '';
 
-		for (var x = 0; x < articles.length; x++) {
-			item = '<a href="' + articles[x].web_url + '"><h3>' + articles[x].headline.main + '</h3></a>';
-			item += '<p>' + articles[x].snippet + '</p>';
-			$nytElem.append(item);
-		}
+	// add address as a parameter for ajax request
+	//console.log(data);
 
-	}).error(function () {
-		$nytHeaderElem.text('Error loading articles');
-	});
+	var articles = data.response.docs;
 
-	// wikipedia request
+	$nytHeaderElem.append(' for ' + fullAddress);
 
-	var wikiRequestTimeout = setTimeout(function () {
-		$wikiElem.text('failed to get wikipedia resources');
-	}, 8000);
+	for (var x = 0; x < articles.length; x++) {
+	    item = '<li><a href="' + articles[x].web_url + '" title="">' + articles[x].headline.main + '</a></li>';
+	    $nytElem.append(item);
+	    $nytElem.children(':last').tooltip({
+		content: articles[x].snippet
+	    });
+	}
 
-	var wikiURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + fullAddress + '&format=json&callback=wikiCallback';
-	$.ajax({
-		url: wikiURL,
-		dataType: 'jsonp',
-		success: function (response) {
-			console.log(response);
-			var articleList = response[1];
+    }).error(function () {
+	$nytHeaderElem.text('Error loading articles');
+    });
 
-			for (var x = 0; x < articleList.length; x++) {
-				var articleStr = articleList[x];
-				var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-				$wikiElem.append('<li><a href="' + url + '">' + articleStr + '</a></li>');
-			}
-			;
-			clearTimeout(wikiRequestTimeout);
-		}
-	});
+    // wikipedia request
 
-	return false;
+    var wikiRequestTimeout = setTimeout(function () {
+	$wikiElem.text('failed to get wikipedia resources');
+    }, 8000);
+
+    var wikiURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + fullAddress + '&format=json&callback=wikiCallback';
+    $.ajax({
+	url: wikiURL,
+	dataType: 'jsonp',
+	success: function (response) {
+	    console.log(response);
+	    var articleList = response[1];
+	    var item = null;
+
+	    for (var x = 0; x < articleList.length; x++) {
+		var articleStr = articleList[x];
+		var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+		$wikiElem.append('<li><a href="' + url + '" title="">' + articleStr + '</a></li>');
+		$wikiElem.children(':last').tooltip({
+		    content: response[2][x]
+		});
+	    }
+	    ;
+	    if (articleList.length === 0) {
+		$wikiElem.append('<li>No Wikipedia articles found for ' + fullAddress + '</li>');
+	    }
+	    clearTimeout(wikiRequestTimeout);
+	}
+    });
+    return false;
 }
 ;
 
 $('#search-form').submit(loadData);
 
 $("#search").dialog({
-	closeOnEscape: false
+    closeOnEscape: false
 });
 
-$("#prior-search-terms").accordion({
-	collapsible: true
+$("#results").accordion({
+    collapsible: true,
+    heightStyle: "fill"
 });
 
 function initialize() {
-	var mapOptions = {
-		location: 'lambertville nj',
-		//center: {lat: -34.397, lng: 150.644},
-		zoom: 8,
-		styles: [{
-				"stylers": [
-					{"saturation": -85},
-					{"gamma": 0.43},
-					{"lightness": -11}
-				]
-			}]
-	};
-	var map = new google.maps.Map(document.getElementById('map-canvas'),
-			mapOptions);
+    var mapOptions = {
+	//location: 'edison nj',
+	center: {lat: -34.397, lng: 150.644},
+	zoom: 8,
+	styles: [{
+		"stylers": [
+		    {"saturation": -65},
+		    {"gamma": 0.43},
+		    {"lightness": -11}
+		]
+	    }]
+    };
+    var map = new google.maps.Map(document.getElementById('map-canvas'),
+	    mapOptions);
 }
 
 initialize();
