a0b3aa52ebc5b6a1602bd387f961dbc80b7d6910
Chris Tirpak <ctirpak1@gmail.com>
2015-03-24 16:45:12 -0400

    simplification of UI

--- p5/js/app.js
+++ p5/js/app.js
@@ -1,113 +1,163 @@
+// this is the model for the data
+var model = {
+	// map options are used to set initial default values and styles
+	mapOptions: {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8,
+		styles: [{
+				"stylers": [
+					{"saturation": -65},
+					{"gamma": 0.43},
+					{"lightness": -11}
+				]
+			}]
+	},
+	// holds currently selected item from results panel
+	selectedMarker: null,
+	// search items and markers
+	'markers': [
+		{
+			'name': 'Marker 1',
+			center: {
+				lat: -34.397,
+				lng: 150.644
+			}
+		},
+		{
+			'name': 'Marker 2',
+			center: {
+				lat: -35.397,
+				lng: 150.644
+			}
+		},
+		{
+			'name': 'Marker 3',
+			center: {
+				lat: -36.397,
+				lng: 150.644
+			}
+		},
+		{
+			'name': 'Marker 4',
+			center: {
+				lat: -37.397,
+				lng: 150.644
+			}
+		},
+		{
+			'name': 'Marker 5',
+			center: {
+				lat: -38.397,
+				lng: 150.644
+			}
+		}
+	]
+};
+
+
+
 
 function loadData() {
 
-    var $wikiElem = $('#wikipedia-links');
-    var $nytHeaderElem = $('#nytimes-header');
-    var $nytElem = $('#nytimes-articles');
-    var fullAddress = '';
-    var nytURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json';
-    var nytParams = {
-	"api-key": "aeacc24cfc3061cdd0c6bba97bbba8ed:5:71450941",
-	"fl": "headline,snippet,web_url"
-    };
-    var historyCount = parseInt($('#history-count').text()) + 1;
-    console.log(historyCount);
+	var $wikiElem = $('#wikipedia-links');
+	var $nytHeaderElem = $('#nytimes-header');
+	var $nytElem = $('#nytimes-articles');
+	var fullAddress = '';
+	var nytURL = 'http://api.nytimes.com/svc/search/v2/articlesearch.json';
+	var nytParams = {
+		"api-key": "aeacc24cfc3061cdd0c6bba97bbba8ed:5:71450941",
+		"fl": "headline,snippet,web_url"
+	};
+	var historyCount = parseInt($('#history-count').text()) + 1;
+	console.log(historyCount);
 
-    // clear out old data before new request
-    $wikiElem.text("");
-    $nytElem.text("");
+	// clear out old data before new request
+	$wikiElem.text("");
+	$nytElem.text("");
 
-    // load streetview
-    fullAddress = $('#street').val();
-    $.extend(nytParams, {'q': fullAddress});
+	// load streetview
+	fullAddress = $('#street').val();
+	$.extend(nytParams, {'q': fullAddress});
 
-    $('#history-items').append('<li>' + fullAddress + '</li>');
-    $('#history-count').text(historyCount);
+	$('#history-items').append('<li>' + fullAddress + '</li>');
+	$('#history-count').text(historyCount);
 
 
-    // NY Times request
-    $.getJSON(nytURL, nytParams, function (data) {
-	var item = '';
+	// NY Times request
+	$.getJSON(nytURL, nytParams, function (data) {
+		var item = '';
 
-	// add address as a parameter for ajax request
-	//console.log(data);
+		// add address as a parameter for ajax request
+		//console.log(data);
 
-	var articles = data.response.docs;
+		var articles = data.response.docs;
 
-	$nytHeaderElem.append(' for ' + fullAddress);
+		$nytHeaderElem.append(' for ' + fullAddress);
 
-	for (var x = 0; x < articles.length; x++) {
-	    item = '<li><a href="' + articles[x].web_url + '" title="">' + articles[x].headline.main + '</a></li>';
-	    $nytElem.append(item);
-	    $nytElem.children(':last').tooltip({
-		content: articles[x].snippet
-	    });
-	}
+		for (var x = 0; x < articles.length; x++) {
+			item = '<li><a href="' + articles[x].web_url + '" title="">' + articles[x].headline.main + '</a></li>';
+			$nytElem.append(item);
+			$nytElem.children(':last').tooltip({
+				content: articles[x].snippet
+			});
+		}
 
-    }).error(function () {
-	$nytHeaderElem.text('Error loading articles');
-    });
+	}).error(function () {
+		$nytHeaderElem.text('Error loading articles');
+	});
 
-    // wikipedia request
+	// wikipedia request
 
-    var wikiRequestTimeout = setTimeout(function () {
-	$wikiElem.text('failed to get wikipedia resources');
-    }, 8000);
+	var wikiRequestTimeout = setTimeout(function () {
+		$wikiElem.text('failed to get wikipedia resources');
+	}, 8000);
 
-    var wikiURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + fullAddress + '&format=json&callback=wikiCallback';
-    $.ajax({
-	url: wikiURL,
-	dataType: 'jsonp',
-	success: function (response) {
-	    console.log(response);
-	    var articleList = response[1];
-	    var item = null;
+	var wikiURL = 'http://en.wikipedia.org/w/api.php?action=opensearch&search=' + fullAddress + '&format=json&callback=wikiCallback';
+	$.ajax({
+		url: wikiURL,
+		dataType: 'jsonp',
+		success: function (response) {
+			console.log(response);
+			var articleList = response[1];
+			var item = null;
 
-	    for (var x = 0; x < articleList.length; x++) {
-		var articleStr = articleList[x];
-		var url = 'http://en.wikipedia.org/wiki/' + articleStr;
-		$wikiElem.append('<li><a href="' + url + '" title="">' + articleStr + '</a></li>');
-		$wikiElem.children(':last').tooltip({
-		    content: response[2][x]
-		});
-	    }
-	    ;
-	    if (articleList.length === 0) {
-		$wikiElem.append('<li>No Wikipedia articles found for ' + fullAddress + '</li>');
-	    }
-	    clearTimeout(wikiRequestTimeout);
-	}
-    });
-    return false;
+			for (var x = 0; x < articleList.length; x++) {
+				var articleStr = articleList[x];
+				var url = 'http://en.wikipedia.org/wiki/' + articleStr;
+				$wikiElem.append('<li><a href="' + url + '" title="">' + articleStr + '</a></li>');
+				$wikiElem.children(':last').tooltip({
+					content: response[2][x]
+				});
+			}
+			;
+			if (articleList.length === 0) {
+				$wikiElem.append('<li>No Wikipedia articles found for ' + fullAddress + '</li>');
+			}
+			clearTimeout(wikiRequestTimeout);
+		}
+	});
+	return false;
 }
 ;
 
 $('#search-form').submit(loadData);
 
-$("#search").dialog({
-    closeOnEscape: false
-});
 
-$("#results").accordion({
-    collapsible: true,
-    heightStyle: "fill"
-});
 
 function initialize() {
-    var mapOptions = {
-	//location: 'edison nj',
-	center: {lat: -34.397, lng: 150.644},
-	zoom: 8,
-	styles: [{
-		"stylers": [
-		    {"saturation": -65},
-		    {"gamma": 0.43},
-		    {"lightness": -11}
-		]
-	    }]
-    };
-    var map = new google.maps.Map(document.getElementById('map-canvas'),
-	    mapOptions);
+	var mapOptions = {
+		center: {lat: -34.397, lng: 150.644},
+		zoom: 8,
+		styles: [{
+				"stylers": [
+					{"saturation": -65},
+					{"gamma": 0.43},
+					{"lightness": -11}
+				]
+			}]
+	};
+	var map = new google.maps.Map(document.getElementById('map-canvas'),
+			mapOptions);
 }
 
 initialize();
