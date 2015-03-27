235438bb5cc3bc357fb370ce3c8f86ba40b57888
Chris Tirpak <ctirpak@users.noreply.github.com>
2015-03-27 14:59:40 -0400

    cleaup

--- p5/js/app.js
+++ p5/js/app.js
@@ -54,7 +54,20 @@ var model = {
 	]
 };
 
-
+function codeAddress() {
+  var geocoder;
+	var address = document.getElementById('address').value;
+  geocoder.geocode( { 'address': address}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      map.setCenter(results[0].geometry.location);
+      var marker = new google.maps.Marker({
+          map: map,
+          position: results[0].geometry.location
+      });
+    } else {
+      alert('Geocode was not successful for the following reason: ' + status);
+    }
+  });
 
 
 function loadData() {
