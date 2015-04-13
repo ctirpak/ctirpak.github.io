566b3fc6dfd42f116fba7fa8e2bda0651adf81fe
Chris Tirpak <ctirpak1@gmail.com>
2015-04-12 21:28:20 -0400

    added click event for map markers

--- p5/js/app.js
+++ p5/js/app.js
@@ -40,6 +40,9 @@ var model = {
 	]
 };
 
+//creates new item which contains
+//location name and
+//google map marker
 var Item = function (data) {
 	this.name = ko.observable(data.name);
 	this.marker = ko.observable(data.marker);
@@ -47,34 +50,63 @@ var Item = function (data) {
 
 
 var viewModel = function () {
+	//capture reference to parent of inner scope items
 	that = this;
+	//create new google map and save it to the model
 	model.map = new google.maps.Map($('#map-canvas')[0], model.mapOptions);
+	//create new geocoder object to be used to translate location names to latLng
 	this.geocoder = new google.maps.Geocoder();
 
+	//array to hold all of the map items
 	this.itemList = ko.observableArray([]);
 
+	//loop through each of the items in teh model and
+	//add markers to the map and
+	//set up click eventListener
 	model.places.forEach(function (oneItem) {
+		//geocode location into latLng
+		//pass location name as first argument
+		//second argument is callback to execute
 		that.geocoder.geocode({'address': oneItem.name}, function (results, status) {
 			if (status == google.maps.GeocoderStatus.OK) {
+				//if status ok, create new marker and save in model
+				//pass arguments of google map and location
 				oneItem.marker = new google.maps.Marker({
 					map: model.map,
 					position: results[0].geometry.location
 				});
+				//add click event listener to marker
+				//centers map and displays info
+				google.maps.event.addListener(oneItem.marker, 'click', function() {
+					model.map.setZoom(12);
+					model.map.setCenter(oneItem.marker.getPosition());
+				});
 				console.log(oneItem);
+				//add to observable array
+				//new item is created that is observable
+				//for the name and google map marker
 				that.itemList.push(new Item(oneItem));
 			} else {
+				//something went wrong
 				alert('Geocode was not successful for the following reason: ' + status);
 				return null;
 			}
 		});
 	});
-
+	
+	//sets the model's current item to the item clicked in the list
 	this.setCurrentItem = function (newItem) {
+		//set model.currentItem
 		model.currentItem = newItem;
+		//animate corresponding google map marker
 		newItem.marker().setAnimation(google.maps.Animation.DROP);
+		//center google map on selected items marker
+		model.map.setCenter(newItem.marker().getPosition());
+		//get and display information about the location
+		//TODO: use ajax to get data from external sites
 	};
 };
 
-
+//initiate knockout with viewModel
 ko.applyBindings(new viewModel);
 
