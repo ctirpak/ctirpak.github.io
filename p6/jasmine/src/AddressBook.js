53bb717ac2d86802e2143fa4e2db267670e767d0
Chris Tirpak <ctirpak1@gmail.com>
2015-04-20 19:06:21 -0400

    updated p6 jasmine test

--- p6/jasmine/src/AddressBook.js
+++ p6/jasmine/src/AddressBook.js
@@ -1,7 +1,27 @@
 function AddressBook() {
-	addContact = function(contact) {
+	this.contacts = [];
+	this.initialComplete = false;
+};
 
-	};
+AddressBook.prototype.addContact = function(contact) {
+	this.contacts.push(contact);
+};
 
+AddressBook.prototype.getContact = function(ContactNum) {
+	return this.contacts[ContactNum];
+}
 
+AddressBook.prototype.deleteContact = function(contactNum) {
+	this.contacts.splice(contactNum, 1);
+};
+
+AddressBook.prototype.getInitialContacts = function(cb) {
+	var self = this;
+	
+	setTimeout(function() {
+		self.initialComplete = true;
+		if (cb) {
+			return cb();
+		}
+	}, 3);
 };
