eddee2fd1adf688c5da6c58c3f1b1940593356b4
Chris Tirpak <ctirpak@users.noreply.github.com>
2017-10-02 23:06:43 -0400

    Update app.js

--- p6/js/app.js
+++ p6/js/app.js
@@ -83,7 +83,7 @@ function loadFeed(id, cb) {
 /* Google API: Loads the Feed Reader API and defines what function
  * to call when the Feed Reader API is done loading.
  */
-google.load('feeds', '1');
+//google.load('feeds', '1');
 google.setOnLoadCallback(init);
 
 /* All of this functionality is heavily reliant upon the DOM, so we
